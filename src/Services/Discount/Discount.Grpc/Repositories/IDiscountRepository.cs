0d74af6791f74ae5bdd2ffeaa2a89f3c2c6926b1
mugrcn <mugrcn@gmail.com>
2022-09-07 11:46:06 +0300

    Discount.API ve Discount.Grpc projeleri olusturuldu.

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon couponName);
+        Task<bool> DeleteDiscount(string productName);
+
+    }
+}
